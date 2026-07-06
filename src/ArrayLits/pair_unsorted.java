b141164b956dd4a5e87bd817afd0e6a93f1e9db5
Pranay-9944 <wanjaripranay9@gmail.com>
2026-07-06 08:10:12 +0530

    pus

--- src/ArrayLits/pair_unsorted.java
+++ src/ArrayLits/pair_unsorted.java
@@ -4,10 +4,34 @@ public class pair_unsorted {
     public static Boolean roated(int[] arr,int target)
     {
 
-        int low=0;
-        int high=arr.length;
+        int pivot=-1;
+        for (int i = 0; i < arr.length-1; i++) {
+            if(arr[i]>arr[i+1])
+            {
+                pivot=i;
+                break;
+            }
+        }
 
-        while(low<high)
+        int low=pivot+1;
+        int high=pivot;
+
+        while(low!=high)
+        {
+            if(arr[low]+arr[high]==target)
+            {
+                return true;
+            }
+            if(arr[low]+arr[high]>target)
+            {
+            high=(arr.length+high- 1)% arr.length;
+            }
+            else
+            {
+                low=(low+1)% arr.length;
+            }
+        }
+        return false;
 
     }
     void main()
@@ -15,7 +39,7 @@ public class pair_unsorted {
 
             int arr[]={11,15,6,8,9,10};
             int target=16;
-            roated(arr);
+            roated(arr,target);
     }
 
 }
