33986707bb9a25f41fb03631a452b8aeb5fbc417
Pranay-9944 <wanjaripranay9@gmail.com>
2026-07-10 22:22:36 +0530

    linkedlist

---
+++ src/Linked_list/LinkedList.java
@@ -0,0 +1,117 @@
+package Linked_list;
+
+public class LinkedList {
+
+        class Node {
+            int data;
+            Node next;
+
+            public Node(int data) {
+                this.data = data;
+                this.next = null;
+
+            }
+
+        }
+    Node head;
+  public void  adFirst(int data)
+  {
+        Node newNode= new Node(data);
+        if(head==null)
+        {
+            head=newNode;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+  }
+
+  public  void Addlast(int data)
+  {
+      Node NewNode=new Node(data);
+      if(head==null)
+      {
+          head=NewNode;
+          return;
+      }
+      Node cur=head;
+          while (cur.next!=null)
+          {
+              cur.next=cur.next;
+          }
+          cur.next=NewNode;
+          NewNode.next=null;
+
+  }
+  public void display()
+  {
+      if(head==null)
+      {
+          System.out.println("list is empty ");
+          return;
+      }
+      Node curr=head;
+      while (curr!=null)
+      {
+          System.out.print(curr.data+"->");
+            curr=curr.next;
+      }
+      System.out.print("null");
+  }
+
+  public void addmiddel(int data,int inx)
+  {
+      Node newnode=new Node(data);
+      if(head==null)
+      {
+          head=newnode;
+          return;
+      }
+      Node cur=head;
+      int i=0;
+      while (i<inx-1)
+      {
+          cur=cur.next;
+          i++;
+
+      }
+      newnode.next=cur.next;
+      cur.next=newnode;
+  }
+  public void delfirst()
+  {
+      if(head==null)
+      {
+          System.out.println("list empty");
+          return;
+      }
+      head=head.next;
+
+
+  }
+  public void dellast()
+  {
+      Node cur=head;
+      Node seclast=head.next;
+      if(head==null)
+      {
+          System.out.println("list empty");
+          return;
+      }
+    while(cur.next!=null)
+    {
+        cur=cur.next;
+        seclast=seclast.next;
+
+    }
+      seclast.next=null;
+   }
+
+    public static void main(String[] args)
+    {
+    LinkedList l=new LinkedList();
+    l.adFirst(10);
+    l.Addlast(11);
+    l.display();
+    }
+}
