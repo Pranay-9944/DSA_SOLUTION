fbde067510a0361585389274b08419cd9823d11c
Pranay-9944 <wanjaripranay9@gmail.com>
2026-07-16 13:15:42 +0530

    puh

--- src/Linked_list/Find_component.java
+++ src/Linked_list/Find_component.java
@@ -1,40 +1,36 @@
-package Linked_list;
-
-import java.util.*;
-
-public class Find_component {
-     public static void find(LinkedList<Integer> list, int[] nums) {
-             int count = 0;
-             HashSet<Integer> set = new HashSet<>();
-             for (int o : nums) {
-                 set.add(o);
-             }
-         for (int i = 0; i < list.size(); i++) {
-
-             if (set.contains(list.get(i)) ) {
-
-                 if(i==list.size()-1)
-                 {
-                   count++;
-                 } else if (!set.contains(list.get(i+1))) {
-                     count++;
-
-                 }
-             }
-
-         }
-         System.out.println(count);
-     }
-
-    public static void main(String[] args) {
-
-        LinkedList<Integer> list = new LinkedList<>();
-
-        for (int i = 5; i > 0; i--) {
-            list.addFirst(i);
-        }
-
-      int[] nums={2,3,4};
-        find(list,nums);
-    }
-}
+//package Linked_list;
+//
+//import java.util.*;
+//
+//public class Find_component {
+//     public static void find(LinkedList<Integer> list, int[] nums) {
+//             int count = 0;
+//             HashSet<Integer> set = new HashSet<>();
+//             for (int o : nums) {
+//                 set.add(o);
+//             }
+//         for (int i = 0; i < list.size(); i++) {
+//
+//             if (set.contains(list.get(i)) ) {
+//
+//                 if(i==list.size()-1 || !set.contains(list.get(i+1))) {
+//                     count++;
+//                 }
+//             }
+//
+//         }
+//         System.out.println(count);
+//     }
+//
+//    public static void main(String[] args) {
+//
+//        LinkedList<Integer> list = new LinkedList<>();
+//
+//        for (int i = 5; i > 0; i--) {
+//            list.addFirst(i);
+//        }
+//
+//      int[] nums={2,3,4};
+//        find(list,nums);
+//    }
+//}
