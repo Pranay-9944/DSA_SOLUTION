7e36a9a443ed9c44a9645992b47c2ee6ef55d880
Pranay-9944 <wanjaripranay9@gmail.com>
2026-07-17 07:26:36 +0530

    add

--- src/Linked_list/LinkedList.java
+++ src/Linked_list/LinkedList.java
@@ -2,240 +2,239 @@ package Linked_list;
 
 public class LinkedList<I extends Number> {
 
-        class Node {
-            int data;
-            Node next;
+    static class Node {
+        int data;
+        Node next;
 
-            public Node(int data) {
-                this.data = data;
-                this.next = null;
-
-            }
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
 
         }
+
+    }
+
     Node head;
-  public void  adFirst(int data)
-  {
-        Node newNode= new Node(data);
-        if(head==null)
-        {
-            head=newNode;
+
+    public void adFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
             return;
         }
-        newNode.next=head;
-        head=newNode;
-  }
+        newNode.next = head;
+        head = newNode;
+    }
 
-  public  void Addlast(int data)
-  {
-      Node NewNode=new Node(data);
-      if(head==null)
-      {
-          head=NewNode;
-          return;
-      }
-      Node cur=head;
-          while (cur.next!=null)
-          {
-              cur.next=cur.next;
-          }
-          cur.next=NewNode;
-          NewNode.next=null;
-
-  }
-  public void display()
-  {
-      if(head==null)
-      {
-          System.out.println("list is empty ");
-          return;
-      }
-      Node curr=head;
-      while (curr!=null)
-      {
-          System.out.print(curr.data+"->");
-            curr=curr.next;
-      }
-      System.out.print("null");
-  }
-
-  public void addmiddel(int data,int inx)
-  {
-      Node newnode=new Node(data);
-      if(head==null)
-      {
-          head=newnode;
-          return;
-      }
-      Node cur=head;
-      int i=0;
-      while (i<inx-1)
-      {
-          cur=cur.next;
-          i++;
-
-      }
-      newnode.next=cur.next;
-      cur.next=newnode;
-  }
-  public void delfirst()
-  {
-      if(head==null)
-      {
-          System.out.println("list empty");
-          return;
-      }
-      head=head.next;
-
-
-  }
-  public void dellast()
-  {
-      Node cur=head;
-      Node seclast=head.next;
-      if(head==null)
-      {
-          System.out.println("list empty");
-          return;
-      }
-    while(cur.next!=null)
-    {
-        cur=cur.next;
-        seclast=seclast.next;
+    public void Addlast(int data) {
+        Node NewNode = new Node(data);
+        if (head == null) {
+            head = NewNode;
+            return;
+        }
+        Node cur = head;
+        while (cur.next != null) {
+            cur.next = cur.next;
+        }
+        cur.next = NewNode;
+        NewNode.next = null;
 
     }
-      seclast.next=null;
-   }
 
-   public void rev()
-   {
-       Node temp=head;
-       Node prev=null;
-       while(temp!=null)
-       {
-            Node front=temp.next;
-            temp.next=prev;
-            prev=temp;
-            temp=front;
-       }
+    public void display() {
+        if (head == null) {
+            System.out.println("list is empty ");
+            return;
+        }
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + "->");
+            curr = curr.next;
+        }
+        System.out.print("null");
+    }
 
-       head = prev;
-   }
-public void delnthnode(int n )
-{
-    int size=0;
-    Node temp=head;
-    while(temp!=null)
-    {
-        temp=temp.next;
-        size++;
+    public void addmiddel(int data, int inx) {
+        Node newnode = new Node(data);
+        if (head == null) {
+            head = newnode;
+            return;
+        }
+        Node cur = head;
+        int i = 0;
+        while (i < inx - 1) {
+            cur = cur.next;
+            i++;
+
+        }
+        newnode.next = cur.next;
+        cur.next = newnode;
+    }
+
+    public void delfirst() {
+        if (head == null) {
+            System.out.println("list empty");
+            return;
+        }
+        head = head.next;
+
 
     }
-    if(n==size)
-    {
-        head=head.next;
+
+    public void dellast() {
+        Node cur = head;
+        Node seclast = head.next;
+        if (head == null) {
+            System.out.println("list empty");
+            return;
+        }
+        while (cur.next != null) {
+            cur = cur.next;
+            seclast = seclast.next;
+
+        }
+        seclast.next = null;
+    }
+
+    public void rev() {
+        Node temp = head;
+        Node prev = null;
+        while (temp != null) {
+            Node front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
+        }
+
+        head = prev;
+    }
+
+    public void delnthnode(int n) {
+        int size = 0;
+        Node temp = head;
+        while (temp != null) {
+            temp = temp.next;
+            size++;
+
+        }
+        if (n == size) {
+            head = head.next;
+            return;
+        }
+        int i = 0;
+        int itofind = size - n;
+        Node prev = head;
+        while (i < itofind) {
+            prev = prev.next;
+            i++;
+        }
+        prev.next = prev.next.next;
         return;
     }
-    int i=0;
-    int itofind=size-n;
-    Node prev=head;
-    while(i<itofind)
-    {
-        prev=prev.next;
-        i++;
+
+    public Node ftm(Node head) {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
     }
-    prev.next=prev.next.next;
-    return;
-}
-public Node ftm(Node head)
-{
-    Node slow=head;
-    Node fast=head;
-    while(fast!=null && fast.next!=null)
-    {
-      slow=slow.next;
-      fast=fast.next.next;
-    }
-    return slow;
-}
-public boolean checkpalin()
-{
-    if(head==null || head.next==null)
-    {
+
+    public boolean checkpalin() {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        Node mid = ftm(head);
+        Node prev = null;
+        Node curr = mid;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+
+
+        }
+        Node right = prev;
+        Node left = head;
+
+
+        while (right != null) {
+            if (left.data != right.data) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+
+        }
         return true;
     }
-    Node mid=ftm(head);
-Node prev=null;
-Node curr=mid;
-Node next;
-while(curr!=null)
-{
-    next=curr.next;
-    curr.next=prev;
-    prev=curr;
-        curr=next;
+
+    public void zigzag(Node head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        Node mid = ftm(head);
+        Node curr = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+
+        }
+        Node right = prev;
+        Node left = head;
+        Node nextl;
+        Node nextr;
+
+        while (left != null && right != null) {
+            nextl = left.next;
+            left.next = right;
+            nextr = right.next;
+            right.next = nextl;
+
+            right = nextr;
+            left = nextl;
+
+        }
 
 
-
-}
-Node right=prev;
-Node left=head;
-
-
-while(right!=null)
-{
-    if(left.data != right.data)
-    {
-        return false;
     }
-    left=left.next;
-    right=right.next;
-
-}
-return true;
-}
-
-public Node zigzag(Node head)
-{
-if(head==null || head.next==null)
-{
-    return head;
-}
-
-Node mid=ftm(head);
-Node curr=mid.next;
- mid.next=null;
-Node prev=null;
-Node next;
-
-while(curr!=null)
-{
-    next=curr.next;
-    curr.next=prev;
-    prev=curr;
-    curr=next;
-
-}
-Node right=prev;
-Node left=head;
-    Node nextl;
-    Node nextr;
-
-while(left!=null && right!=null)
-{
-    nextl=left.next;
-    left.next=right;
-  nextr=right.next;
-    right.next=nextl;
-
-  right=nextr;
-  left=nextl;
-
-}
 
 
-}
+    public static Node odd_even(Node head) {
+        if (head == null || head.next == null) {
+            return head;
 
+            Node odd = head;
+            Node even = head.next;
+            Node evenHead = even;
+
+
+            while (even != null && even.next != null) {
+                odd.next = even.next;
+                odd = odd.next;
+                even.next = odd.next;
+                even = even.next;
+
+            }
+            odd.next = evenHead;
+
+
+        }
+
+
+        return head;
+    }
 
     public static void main(String[] args)
     {
@@ -243,6 +242,6 @@ while(left!=null && right!=null)
     l.adFirst(10);
         l.adFirst(20);
         l.adFirst(10);
-        System.out.println( l.checkpalin());;
+       l.odd_even(l.head);
     }
 }
---
+++ src/Linked_list/odd_even.java
@@ -0,0 +1,27 @@
+//package Linked_list;
+//
+//import java.util.LinkedList;
+//
+//public class odd_even {
+//    public static void odd_even(LinkedList l)
+//    {
+//
+//
+//    }
+//    void main() {
+//
+//LinkedList<Integer> a=new LinkedList<>();
+//a.addFirst(1);
+//a.addLast(2);
+//a.addLast(3);
+//a.addLast(4);
+//a.addLast(5);
+//for (int i:a )
+//{
+//    System.out.println(i);
+//}
+//odd_even(a);
+//
+//
+//    }
+//}
