56d9f52a5b132478a2127aab48b68086560db85d
Pranay-9944 <wanjaripranay9@gmail.com>
2026-07-27 10:48:39 +0530

    puhs

---
+++ src/STACK_PACK/Duplictae.java
@@ -0,0 +1,36 @@
+package STACK_PACK;
+
+
+import java.util.Stack;
+
+public class Duplictae {
+    public static  boolean duplicate_parenthesis(String a)
+    {
+        Stack<Character> s=new Stack<>();
+        for (int i = 0; i < a.length(); i++) {
+            char c=a.charAt(i);
+            if(c==')') {
+                int count = 0;
+                while (s.peek() != '(') {
+                    s.pop();
+                    count++;
+                }
+
+                if (count<1) {
+                    return true;
+                } else {
+                    s.pop();
+                }
+            }
+            else {
+                s.push(c);
+            }
+        }
+        return  false;
+    }
+    void main()
+    {
+        String a="(A+b)";
+        System.out.println(   duplicate_parenthesis(a));;
+    }
+}
