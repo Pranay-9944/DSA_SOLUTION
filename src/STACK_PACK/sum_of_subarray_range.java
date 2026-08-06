279583ea3a1e5c12ac58dd1f1489c86d6429c00c
Pranay-9944 <wanjaripranay9@gmail.com>
2026-08-06 08:11:56 +0530

    brute force

---
+++ src/STACK_PACK/sum_of_subarray_range.java
@@ -0,0 +1,29 @@
+package STACK_PACK;
+
+public class sum_of_subarray_range {
+    public  static  void range(int a[]){
+
+        int sum=0;
+
+
+        for (int i = 0; i <a.length ; i++) {
+            int small=a[i];
+            int large=a[i];
+
+            for (int j = i+1; j < a.length; j++) {
+           small=Math.min(small,a[j]);
+           large=Math.max(large,a[j]);
+
+           sum =sum+(large-small);
+             }
+
+        }
+        System.out.println(sum);
+    }
+    void main()
+    {
+        int a[]={1,3,3};
+        System.out.println(a.length);
+        range(a);
+    }
+}
