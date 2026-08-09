8ae16e0e8a9195ae7eb90f08fc144bab349e9f7b
Pranay-9944 <wanjaripranay9@gmail.com>
2026-08-09 11:51:51 +0530

    optimal

--- src/STACK_PACK/Sliding_window_max.java
+++ src/STACK_PACK/Sliding_window_max.java
@@ -1,24 +1,55 @@
 package STACK_PACK;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.RandomAccess;
+
 public class Sliding_window_max {
     public static  void maximum(int a[])
     {
+//        int k=3;
+//        int ans[] =new int[a.length-k+1];
+//        for (int i = 0; i <=a.length-k; i++) {
+//
+//                    int max = Math.max(a[i], Math.max(a[i + 1], a[i + 2]));
+//                    ans[i]=max;
+//
+//
+//
+//
+//
+//        }
+//        for(int i:ans)
+//        {
+//            System.out.println(i);
+//        }
+
         int k=3;
-        int ans[] =new int[a.length-k+1];
-        for (int i = 0; i <=a.length-k; i++) {
+        int ans[]=new int[a.length-k+1];
+        Deque<Integer> d= new ArrayDeque<>();
+        for (int i = 0; i < a.length; i++) {
 
-                    int max = Math.max(a[i], Math.max(a[i + 1], a[i + 2]));
-                    ans[i]=max;
+            while(!d.isEmpty() && d.peekFirst() <=i-k)
+            {
+                d.removeFirst();
 
-
-
+            }
+            while(!d.isEmpty() && a[d.peekLast()]<a[i])
+            {
+                d.removeLast();
+            }
+            d.addLast(i);
+            if (i >= k - 1) {
+                ans[i - k + 1] = a[d.peekFirst()];
+            }
 
 
         }
-        for(int i:ans)
+        for (int b:ans)
         {
-            System.out.println(i);
+            System.out.println(b);
         }
+
 
     }
     void main()
