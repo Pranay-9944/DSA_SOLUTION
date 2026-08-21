a29f92ad10b208bb7b70016b5380a043c1c2e313
Pranay-9944 <wanjaripranay9@gmail.com>
2026-08-21 08:14:12 +0530

    push

---
+++ src/Greedy/prob_435_non_overlapping.java
@@ -0,0 +1,31 @@
+package Greedy;
+
+import java.util.Arrays;
+
+public class prob_435_non_overlapping {
+    void main()
+    {
+        int[][] pairs = {
+                {1, 2},
+                {2, 3},
+                {3, 4},
+                {1,3}
+        };
+
+        Arrays.sort(pairs ,(a,b)->Integer.compare(a[1],b[1]));
+
+
+        int count=pairs.length-1;
+        int t=pairs[0][1];
+        for (int i = 1; i < pairs.length; i++) {
+            if(pairs[i][0]>=t)
+            {
+                count--;
+                t=pairs[i][1];
+
+            }
+
+        }
+        System.out.println(count);
+    }
+}
