95c8d632f7c0cf753df9968c392057b6a5e3d8ef
Pranay-9944 <wanjaripranay9@gmail.com>
2026-09-17 23:55:21 +0530

    push

--- src/Greedy/Mini_abs_pair.java
+++ src/Greedy/Mini_abs_pair.java
@@ -4,18 +4,20 @@ import java.util.Arrays;
 
 public class Mini_abs_pair {
 
-    void main()
-    {
-        int a[]={1,2,3};
-        int b[]={2,1,3};
-        int sum=0;
-        Arrays.sort(a);
-        Arrays.sort(b);
-        for (int i = 0; i < a.length; i++) {
+            void main()
+            {
+                int a[]={1,2,3};
+                int b[]={2,1,3};
+                int sum=0;
+                Arrays.sort(a);
+                Arrays.sort(b);
+                for (int i = 0; i < a.length; i++) {
 
-           sum+= Math.abs(a[i]-b[i]);
+                   sum+= Math.abs(a[i]-b[i]);
 
+                }
+                System.out.println(sum);
+                System.out.println(sum);
+
+            }
         }
-        System.out.println(sum);
-    }
-}
